51a0e7c0b87e82bb4b5337076c38f56f644a5457
stjdl <91888318+stjdl@users.noreply.github.com>
2023-01-31 14:24:22 +0100

    Add files via upload

--- src/main/java/gestionfp/repository/RolesRepository.java
+++ src/main/java/gestionfp/repository/RolesRepository.java
@@ -1,12 +1,18 @@
 package gestionfp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import gestionfp.model.Rol;
 
 @Repository
 public interface RolesRepository extends JpaRepository<Rol, Long> {
-
+	@Query(
+			value="SELECT * FROM roles WHERE nombre = ?1 ",
+			nativeQuery=true)
+	Optional<Rol> getByName(String name);
 }
 
